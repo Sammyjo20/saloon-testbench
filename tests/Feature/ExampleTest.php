653d22e3c536cdeb5ef920a353736c34add61130
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-11-07 18:41:57 +0000

    wip

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature;
 use App\Http\Integrations\Test\Requests\TestRequest;
 use App\Http\Integrations\Test\TestConnector;
 use Generator;
+use Psr\Http\Message\RequestInterface;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -24,23 +25,45 @@ class ExampleTest extends TestCase
     {
         $connector = new TestConnector;
 
+        $connector->sender()->addMiddleware(function (callable $handler) {
+            return function (RequestInterface $request, array $options) use ($handler) {
+                // dump('Send Guzzle!');
+
+                return $handler($request, $options);
+            };
+        });
+
+        $connector->middleware()->onRequest(function () {
+             // dump('Send!');
+        });
+
+        $connector->middleware()->onResponse(function () {
+            // dump('response');
+        });
+
         $requests = function () {
-            for ($i = 0; $i < 100; $i++) {
+            for ($i = 0; $i < 1000; $i++) {
                 yield new TestRequest;
             }
         };
 
-        $pool = $connector->pool($requests(), 10);
+        $pool = $connector->pool($requests(), 20);
 
         $count = 0;
+        $responses = [];
 
-        $pool->withResponseHandler(function ($value) use (&$count) {
-            // dump($value);
+        $pool->withResponseHandler(function ($value) use (&$count, &$responses) {
+            $responses[] = $value?->body();
             $count++;
+        });
+
+        $pool->withExceptionHandler(function ($value) use (&$count) {
+            ray($value);
         });
 
         $pool->send()->wait();
 
         dump($count);
+        dump($responses);
     }
 }
